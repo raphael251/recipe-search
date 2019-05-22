904dd240d3e51799e3e5754ec3f8b65e605a9c81
Raphael Passos <raphael251@hotmail.com>
2019-05-22 11:23:23 -0300

    Updates

---
+++ precache-manifest.242e2508bdc1f304818144e3168fb8ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bc8c6397ec0b07f700bd387a1da78cd",
+    "url": "/recipe-search/index.html"
+  },
+  {
+    "revision": "aead8cb40786c21dcb81",
+    "url": "/recipe-search/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "31e6432514d672c68bdc",
+    "url": "/recipe-search/static/css/main.1df341b5.chunk.css"
+  },
+  {
+    "revision": "aead8cb40786c21dcb81",
+    "url": "/recipe-search/static/js/2.f9b7975b.chunk.js"
+  },
+  {
+    "revision": "31e6432514d672c68bdc",
+    "url": "/recipe-search/static/js/main.78bd7ac1.chunk.js"
+  },
+  {
+    "revision": "fe6af794f5e4f8da6582",
+    "url": "/recipe-search/static/js/runtime~main.b26d42ce.js"
+  }
+]);
